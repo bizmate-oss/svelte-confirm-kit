270af6592a72e015fe9b6ed3b4159b92bbf402bd
Francesco Boscarino <francesco@bizmate.it>
2022-08-05 16:19:22 +0200

    Porting to SvelteKit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+import Confirm from '$lib/Confirm.svelte';
+
+function deleteItem() {
+    alert('Deleted')
+}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<Confirm confirmTitle="Delete" cancelTitle="Cancel" let:confirm="{confirmThis}">
+    <button on:click={() => confirmThis(deleteItem)}>Delete</button>
+</Confirm>
